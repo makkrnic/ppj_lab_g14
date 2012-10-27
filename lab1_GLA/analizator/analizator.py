9e705cf85b90e41c7ef985a8014fbf9bb1838b2e
Mak Krnic <mkrnic@gmail.com>
2012-10-27 16:13:44 +0200

    -

--- lab1_GLA/analizator/analizator.py
+++ lab1_GLA/analizator/analizator.py
@@ -12,30 +12,72 @@ class Re2enka ():
     self.stanja = set([self.pocetno, self.ciljno])
     self.re2enka2 (re, self.pocetno, self.ciljno)
     
-    print (self.prijelazi)
+    #print ('prijelazi: ' + str(self.prijelazi) + '\n')
+
+    
+    while True:
+      provjereno_prijelaza = 0
+      for p in self.prijelazi:
+        provjereno_prijelaza += 1
+        (s, z) = p
+        if z != 'eps' and len(z) > 1 and z[0] != '\\':
+          #print ('prijelaz: ' + str (p) + ' => ' + str(self.prijelazi[p]) + ' nije ok')
+          #print ('nije ok')
+          self.popravi_prijelaz (p, self.prijelazi[p])
+          break
+        #else:
+        #  #debug
+        #  print ('ok')
+      
+      if provjereno_prijelaza == len (self.prijelazi):
+        break
+    
+    print ('prijelazi Novi: ' + str(self.prijelazi) + '\n')
   
   def novo_stanje (self):
     self.stanja.add(self.zadnje_stanje)
     self.zadnje_stanje += 1
     return self.zadnje_stanje - 1
+
+
+  def flatten(self, x):
+      result = []
+      for el in x:
+          if hasattr(el, "__iter__") and not isinstance(el, str):
+              result.extend(self.flatten(el))
+          else:
+              result.append(el)
+      return result
   
-  def dodaj_prijelaz (self, stanje, znak, novo_stanje):        
+  def dodaj_prijelaz (self, stanje, znak, novo_stanje):
+    if not znak:
+      znak = 'eps'
+      return
+    
+    #print ('Dodajem: ' + str((stanje, znak)) + ' => ' + str (novo_stanje))
 
     if (stanje, znak) not in list (self.prijelazi.keys()):
-      self.prijelazi[(stanje, znak)] = [novo_stanje]
+      novo_stanje = [novo_stanje]
+      self.prijelazi[(stanje, znak)] = self.flatten(novo_stanje)
     else:
       self.prijelazi[(stanje, znak)].append (novo_stanje)
  
   def popravi_prijelaz (self, prijelazIz, prijelazU):
+    #print ('popravljam prijelaz: ' + str (prijelazIz) + ' => ' + str(prijelazU))
+
     (stanjeIz, znak) = prijelazIz
     stanjaU = prijelazU
+    #print ('Brisem: ' + str (prijelazIz) + ' => ' + str(self.prijelazi[prijelazIz]))
     del self.prijelazi[prijelazIz]
-
+    #print ('Prijelazi temp: ' + str (self.prijelazi))
+    #print ('Rjesavam regex: ' + znak + '\n')
+    
+    #print ('Ulazi u re2enka2: '+ str(znak) + ', ' + str (stanjeIz) + ', '+ str(stanjaU) + '\n')
     self.re2enka2 (znak, stanjeIz, stanjaU)
   
   
   def re2enka2 (self, re, pStanje, cStanje):
-    print ('regex: ' + re)
+    #print ('regex: ' + re + '\nPocetno: ' + str(pStanje) + '\nCiljno: ' + str (cStanje) + '\n')
     posebni = ['|', '(', ')', '*']
     posebni2 = {'_': ' ', 'n': "\n", 't': "\t", '$': ''}
   
@@ -71,11 +113,15 @@ class Re2enka ():
           lijevo = lijevo[1:-1]
         
         if len(reList) -1 <= i:
-          s1 = self.novo_stanje()
-          print ('zadnji: ' + self.zadnjiOper)
-          if self.zadnjiOper == '':
+          #s1 = self.novo_stanje()
+          #print ('zadnji: ' + self.zadnjiOper)
+          #print ('desno:')
+          #print (desno)
+          #if self.zadnjiOper == '':
             #self.prijelazi[(cStanje, ''.join(lijevo))] = [s1]
-            self.dodaj_prijelaz (cStanje, ''.join(lijevo), s1)
+
+          #print ('Ciljno: ' + str (cStanje))
+          self.dodaj_prijelaz (pStanje, ''.join(lijevo), cStanje)
           break 
 
         elif reList[i+1] == '*':
@@ -96,8 +142,8 @@ class Re2enka ():
           if len (lijevo) > 1:
             self.popravi_prijelaz ((s1, ''.join(lijevo)), s2)
 
-          if len (desno) > 1:
-            self.popravi_prijelaz ((cStanje, ''.join(desno)), s3)
+#          if len (desno) > 1:
+#            self.popravi_prijelaz ((cStanje, ''.join(desno)), s3)
 
           self.zadnjiOper = '*'
         elif reList[i+1] == '|':
@@ -109,8 +155,8 @@ class Re2enka ():
           
           if len (lijevo) > 1:
             self.popravi_prijelaz ((pStanje, ''.join(lijevo)), cStanje)
-          if len (desno) > 1:
-            self.popravi_prijelaz ((pStanje, ''.join(desno)), cStanje)
+          #if len (desno) > 1:
+          #  self.popravi_prijelaz ((pStanje, ''.join(desno)), cStanje)
           self.zadnjiOper = '|'
         else:
           #nadovezivanje
@@ -123,8 +169,8 @@ class Re2enka ():
           if len (lijevo) > 1:
             self.popravi_prijelaz ((pStanje, ''.join(lijevo)), s1)
 
-          if len (desno) > 1:
-            self.popravi_prijelaz ((s1, ''.join(desno)), cStanje)
+          #if len (desno) > 1:
+          #  self.popravi_prijelaz ((s1, ''.join(desno)), cStanje)
           self.zadnjiOper = ''
         
        # if len (lijevo) < 2:
@@ -146,68 +192,6 @@ class Re2enka ():
       
       i += 1
   
-    #print (lijevo)
-    #print (desno)
-    #print (self.stanja)
-  
-  
-  #  for i in range (0, len (re)):
-  #  while i < len (a):
-  #    znak = re[i]
-  #    if znak == '\\' and not slash:
-  #      slash = True
-  #      continue
-  #
-  #    if slash and (znak in posebni2):
-  #      znak = posebni2[znak]
-  #    elif not slash and (znak in posebni):
-  #      if znak == '(':
-  #        zagrade += 1
-  #        stanjaIspredZagrada.append (stanje)
-  #        continue
-  #      elif znak == ')':
-  #        zagrade -= 1
-  #        prethodnoZagrada = True
-  #        continue
-  #      elif znak == '*':
-  #        kleen = True
-  #      elif znak == '|':
-  #        ili = True
-        
-  #    if prethodnoZagrada:
-  #      prethodnoStanje = stanjaIspredZagrada.pop()
-  #      prethodnoZagrada = False
-  #    else:
-  #      prethodnoStanje = stanje
-  #
-  #    stanje += 1
-  #    stanja.add (stanje)
-  #
-  #    if kleen:
-  #      kleen = False
-  #      if prijelazi.get((prethodnoStanje, 'eps'), False):
-  #        prijelazi[(prethodnoStanje, 'eps')].append(stanje)
-  #      else:
-  #        prijelazi[(prethodnoStanje, 'eps')] = [stanje]
-  #
-  #      continue
-  #      
-  #      
-  #
-  #    slash = False
-  #
-  #    
-  #    if prijelazi.get((stanje - 1, znak), False):
-  #      prijelazi[(stanje - 1, znak)].append(stanje)
-  #    else:
-  #      prijelazi[(stanje - 1, znak)] = [stanje]
-  #
-  #    i += 1
-    
-  #  print ('Stanja: '); print (stanja)
-  #  print ('Prijelazi: '); print(prijelazi)
-
-
 def re2enka (re, pocetno):
   r2e = Re2enka(re, pocetno)
   return 
